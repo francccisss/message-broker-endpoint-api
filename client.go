b32fecf16ce42cc77eafaae7292c01f4f94d8117
francis <franzdiaz460@gmail.com>
2025-02-04 02:17:10 +0400

    ref: removed listener

--- client.go
+++ client.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"encoding/binary"
 	"fmt"
-	"math"
 	"net"
 
 	"github.com/google/uuid"
@@ -31,7 +27,7 @@ func Connect(address string) (connection, error) {
 		fmt.Println(err.Error())
 		return connection{}, err
 	}
-	go HandleIncomingRequests(conn)
+	go Mudem(conn)
 	fmt.Printf("NOTIF: Successfully Connected to message broker on %s\n", address)
 	return connection{conn}, nil
 }
@@ -66,58 +62,4 @@ func (c connection) CreateChannel() (Channel, error) {
 		STREAM_POOL[newStreamID] = &ch
 	}
 	return ch, nil
-}
-
-// TODO Update this using using the server's implementation of handling
-// of incoming requests
-func HandleIncomingRequests(c net.Conn) {
-	connBufReader := bufio.NewReader(c)
-	defer c.Close()
-
-	// fixed sized header length to extract from message stream
-	// Outer loop will always take 4 iterations
-	headerBuf := make([]byte, HEADER_SIZE)
-	readSize := DEFAULT_READ_SIZE
-	for {
-		var msgBuf bytes.Buffer
-		_, err := c.Read(headerBuf)
-		if err != nil {
-			fmt.Println("ERROR: Unable to decode header prefix length")
-			return
-		}
-
-		expectedMsgLength := int(binary.LittleEndian.Uint32(headerBuf[:HEADER_SIZE]))
-		fmt.Printf("Prefix Length Receieved: %d\n", expectedMsgLength)
-		for {
-			bodyBuf := make([]byte, readSize)
-			_, err := connBufReader.Read(bodyBuf)
-			if err != nil {
-				fmt.Printf("ERROR: Unable to read the incoming message body ")
-				break
-			}
-
-			// store bytes from stream up to the current readsize length into the
-			// msgBuf (msgBuf is the current accumulated requested stream from client)
-			_, err = msgBuf.Write(bodyBuf[:])
-			if err != nil {
-				fmt.Printf("ERROR: Unable to write incoming bytes to the message buffer ")
-				break
-			}
-
-			// Updates the readsize for the next stream of bytes to be captured in bulk
-			// This formula returns the minimum int between the two, if there is space
-			// to fit the stream of bytes in the bodyBuf then return current readSize which is the DEFAULT_READ_SIZE
-			// else if current readSize is greater than the remaining bytes left from the expected message
-			// return n bytes up to the length of the remaining bytes of the current message.
-			readSize = int(math.Min(float64(expectedMsgLength-msgBuf.Len()), float64(readSize)))
-
-			// finishes the current stream request
-			if msgBuf.Len() == expectedMsgLength {
-				readSize = DEFAULT_READ_SIZE
-				fmt.Println("NOTIF: Message sequence complete.")
-				fmt.Println("NOTIF: Do something with the new request.")
-				break
-			}
-		}
-	}
 }
